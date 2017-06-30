a3f1abe5125a5892575193914a5b0b3bf2789c44
Ann Prokhorova <prokhorova.ann26@gmail.com>
2017-06-30 22:57:31 +0400

    delete commented code

--- server/config/express.js
+++ server/config/express.js
@@ -1,14 +1,9 @@
 var express = require('express');
-// var session = require('express-session');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var compress = require('compression');
 var expressValidator = require('express-validator');
 var cors = require('cors');
-// var mongoose = require('mongoose');
-// var MongoStore = require('connect-mongo')(session);
-// var passport = require('passport');
-
 var config = require('./config.js');
 
 module.exports = function() {
@@ -26,16 +21,6 @@ module.exports = function() {
     } else {
         app.use(morgan('dev'));
     }
-
-    // app.use(session({
-    //     secret: config.sessionSecret,
-    //     resave: true,
-    //     saveUninitialized: true,
-    //     store: new MongoStore({mongooseConnection: mongoose.connection})
-    // }));
-
-    // app.use(passport.initialize());
-    // app.use(passport.session());
 
     require('../app/routes/router.js')(app);
 
