d6701a668bd5b09048069a43dacc0973805f5c41
Ann Prokhorova <prokhorova.ann26@gmail.com>
2017-07-16 02:11:29 +0400

    delete usused stuff

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,2 @@
-
 module.exports = require('./env/' + process.env.NODE_ENV + '.js');
 
--- server/server.js
+++ server/server.js
@@ -3,13 +3,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var mongoose = require('./config/mongoose.js');
 var express = require('./config/express.js');
-// var passport = require('./server/config/passport.js');
 
 var port = process.env.PORT || 9000;
 
 var db = mongoose();
 var app = express();
-// var passport = passport();
 
 app.listen(port, function() {
     console.log('Server listening on port ' + port);
